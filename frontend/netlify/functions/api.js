2a81f5fde64361f68794f8e66dde9212faeb8ad4
Scott-Pickett6 <156327048+Scott-Pickett6@users.noreply.github.com>
2025-03-04 17:38:10 +0000

    added files

---
+++ frontend/netlify/functions/api.js
@@ -0,0 +1,33 @@
+import express from "express";
+import serverless from "serverless-http";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+const api = express();
+const router = express.Router();
+
+router.get("/recommendations", async (req, res) => {
+    const artist = req.query.artist;
+
+    if (!artist) return res.status(400).json({
+        error: "Artist is required"
+    });
+    try {
+        const response = await fetch(
+            `https://musicbrainz.org/ws/2/artist/?query=artist:${encodeURIComponent(artist)}fmt=json`,
+            {
+                headers: {
+                    Accept: "application/json"
+                }
+            }
+        );
+        const data = await response.json();
+        console.log("data: " + data);
+    } 
+    catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+api.use("/api/", router);
+export const handler = serverless(api);
